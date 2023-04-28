43b8f50126544f08041fc061dc69e6fa1fcef30f
James Koduah <jameskoduah171@gmail.com>
2023-04-28 12:05:33 +0000

    0-create_a_file.pp: puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file
 
 file {'/tmp/school':
-	ensure => 'present',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet'
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
