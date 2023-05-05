1d26616437001bdfbf078701f6adff9227bd6744
James Koduah <jameskoduah171@gmail.com>
2023-05-05 17:00:50 +0000

    test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,12 +1,10 @@
 exec { 'add nginx stable repo':
   command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 # update software packages list
 exec { 'update packages':
   command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 # install nginx
@@ -17,14 +15,12 @@ package { 'nginx':
 # allow HTTP
 exec { 'allow HTTP':
   command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
 }
 
 # change folder rights
 exec { 'chmod www folder':
   command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 # create index file
