86d46e3e92ae4a60c6f32b33f2053c2e436df0aa
James Koduah <jameskoduah171@gmail.com>
2023-05-09 13:20:36 +0000

    2-puppet_custom_http_response_header.pp: first

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+Exec {
+  path => ['/bin', '/usr/bin'],
+}
+
+exec { 'add nginx header' :
+  command => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/server_name _;/a add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart
+}
