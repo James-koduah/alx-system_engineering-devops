40b956cabbebdc19e1b694b2017da665857c372e
James Koduah <jameskoduah171@gmail.com>
2023-08-11 12:11:06 +0000

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix apache wordpress site
 exec { 'fix':
-  command => 'bash -c "sed -i s/.phpp/.php /var/www/html/wp-settings.php"',
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php /var/www/html/wp-settings.php"',
   path    => '/usr/bin:/usr/sbin:/bin'
 }
